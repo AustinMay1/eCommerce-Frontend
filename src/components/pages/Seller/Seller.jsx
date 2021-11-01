75050121fc0e7fe6d0538e03cd05f514a36679b9
Alexander Messier <alexander.j.messier@gmail.com>
2021-11-01 01:31:14 -0500

    Converted seller.jsx into a hook form. Linked the user back to the home page after creating a product.

--- src/components/pages/Seller/Seller.jsx
+++ src/components/pages/Seller/Seller.jsx
@@ -1,93 +1,73 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React from "react";
 import "./seller.css"
+import axios from 'axios';
+import useForm from '../../../hooks/useForm';
+import { useHistory } from "react-router-dom";
 
-class Seller extends Component {
-    constructor(props) {
-        super(props)
 
-        this.state = {
-            name: '',
-            category: 'Hoodies',
-            description: '',
-            price: 20,
+const Seller = () => {
+
+    const { formValues, handleChange, handleSubmit } = useForm(submitProduct)
+    const history = useHistory();
+
+    function submitProduct(){
+        let priceAsNumber = parseFloat(formValues.price);
+        let newProduct = {
+            name: formValues.name,
+            category: formValues.category,
+            description: formValues.description,
+            price: priceAsNumber
         }
+        createProduct(newProduct);
+        history.push('/')
     }
 
-    handleProductChange = (event) => {
-        this.setState({
-            name: event.target.value
-        })
-    }
-    handleCategoryChange = (event) => {
-        this.setState({
-            category: event.target.value
-        })
-    }
-    handleDescriptionChange = (event) => {
-        this.setState({
-            description: event.target.value
-        })
-    }
-    handlePriceChange = (event) => {
-        this.setState({
-            price: event.target.value
-        })
-    }
-    handleSubmit = (event) => {
-        event.preventDefault()
-        this.createProduct(this.state)
-    }
-    
-    createProduct = async (newProduct) => {
+    const createProduct = async (productObject) => {
         try{
-            let object=this.state
-            await axios.post("https://localhost:44394/api/products", object);
+            let newProduct = productObject;
+            await axios.post("https://localhost:44394/api/products", newProduct);
         }
             
         catch(ex){
             console.log('Error in createProduct API call', ex)
         }
-    }     
+    }
 
-    render() {
-        return (
-            <div className="body">
+    return (
+        <div className="body">
             <div className= 'container'>
                 <div className ="title">Create a Product Listing</div>
-            <form className = "-container" onSubmit={this.handleSubmit}>
-                <div className ="user-details">
-                    <div className="input-box">
-                    <span className="details">Product Name</span>
-                    <input type='text'  value={this.state.name} onChange={this.handleProductChange}/>
-                </div>
-                <div className="input-box">
-                <span className="details">Category</span>
-                    <select value={this.state.category} onChange={this.handleCategoryChange}>
-                        <option value="hoodies">Hoodies</option>
-                        <option value="pants">Pants</option>
-                        <option value="shirts">Shirts</option>
-                        <option value="hats">Hats</option>
-                        <option value="jackets">Jackets</option>
-                    </select>
-                </div>
-                <div className="input-box">
-                <span className="details">Description</span>
-                <input type='text' value={this.state.description} onChange={this.handleDescriptionChange}/>
-                </div>
-                <div className="input-box">
-                <span className="details">Price</span>
-                    <input type="number" id="number" name="price" value={this.state.price} onChange={this.handlePriceChange}/>
-                </div>
-                
-                <button className="button" type="submit">Submit</button>
-            
-                </div>
-               
+                <form className = "-container" onSubmit={handleSubmit}>
+                    <div className ="user-details">
+                        <div className="input-box">
+                            <span className="details">Product Name</span>
+                            <input type='text' name='name' value={formValues.name} onChange={handleChange}/>
+                        </div>
+                        <div className="input-box">
+                            <span className="details">Category</span>
+                            <select name='category' value={formValues.category} onChange={handleChange}>
+                                <option value=""></option>
+                                <option value="Hoodies">Hoodies</option>
+                                <option value="Pants">Pants</option>
+                                <option value="Shirts">Shirts</option>
+                                <option value="Hats">Hats</option>
+                                <option value="Jackets">Jackets</option>
+                            </select>
+                        </div>
+                        <div className="input-box">
+                            <span className="details">Description</span>
+                            <input type='text' name='description' value={formValues.description} onChange={handleChange}/>
+                        </div>
+                        <div className="input-box">
+                            <span className="details">Price</span>
+                            <input type="number" id="number" name="price" value={formValues.price} onChange={handleChange}/>
+                        </div>
+                        <button className="button" type="submit">Submit</button>
+                    </div>
                 </form>
             </div>
-         </div>
-        );
-    }
+        </div>
+    );
 }
+
 export default Seller;
